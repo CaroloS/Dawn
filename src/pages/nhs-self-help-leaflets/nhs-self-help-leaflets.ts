0767a9db2eb55d8bda4313d2cd7322a20852a2fa
Caroline Smith <caroline.smith87@gmail.com>
2017-11-22 20:39:48 +0000

    changed directory links to to inApp rather than external

--- src/pages/nhs-self-help-leaflets/nhs-self-help-leaflets.ts
+++ src/pages/nhs-self-help-leaflets/nhs-self-help-leaflets.ts
@@ -13,7 +13,7 @@ export class NhsSelfHelpLeafletsPage {
     hidden : 'no', //Or  'yes'
     clearcache : 'yes',
     clearsessioncache : 'yes',
-    zoom : 'yes',//Android only ,shows browser zoom controls 
+    zoom : 'yes',//Android only ,shows browser zoom controls
     hardwareback : 'yes',
     mediaPlaybackRequiresUserAction : 'no',
     shouldPauseOnSuspend : 'no', //Android only
@@ -29,8 +29,18 @@ export class NhsSelfHelpLeafletsPage {
   constructor(private theInAppBrowser: InAppBrowser) {
   }
 
-  public openWithCordovaBrowser(url : string){
+  public openWithCordovaBrowser(url : string, ){
     let target = "_self";
+    this.theInAppBrowser.create(url,target,this.options);
+}
+
+public openWithSystemBrowser(url : string){
+    let target = "_system";
+    this.theInAppBrowser.create(url,target,this.options);
+}
+
+public openWithInAppBrowser(url : string){
+    let target = "_blank";
     this.theInAppBrowser.create(url,target,this.options);
 }
 
