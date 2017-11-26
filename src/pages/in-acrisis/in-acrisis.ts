095cc4ce81376132de1dcb2db84047937306aa84
j4sond3ak1n <jason.deakin.dev@gmail.com>
2017-11-26 01:21:55 +0000

    styling and fixes

--- src/pages/in-acrisis/in-acrisis.ts
+++ src/pages/in-acrisis/in-acrisis.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser , InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-in-acrisis',
   templateUrl: 'in-acrisis.html'
 })
 export class InACrisisPage {
-
-  constructor(public navCtrl: NavController) {
+  options : InAppBrowserOptions = {
+    location : 'yes',//Or 'no'
+    hidden : 'no', //Or  'yes'
+    clearcache : 'yes',
+    clearsessioncache : 'yes',
+    zoom : 'yes',//Android only ,shows browser zoom controls
+    hardwareback : 'yes',
+    mediaPlaybackRequiresUserAction : 'no',
+    shouldPauseOnSuspend : 'no', //Android only
+    closebuttoncaption : 'Close', //iOS only
+    disallowoverscroll : 'no', //iOS only
+    toolbar : 'yes', //iOS only
+    enableViewportScale : 'no', //iOS only
+    allowInlineMediaPlayback : 'no',//iOS only
+    presentationstyle : 'pagesheet',//iOS only
+    fullscreen : 'yes',//Windows only
+};
+  constructor(private theInAppBrowser: InAppBrowser) {
   }
-  
+  public openWithInAppBrowser(url : string){
+      let target = "_blank";
+      this.theInAppBrowser.create(url,target,this.options);
+	  }
 }
