612436f35540893232cc4e2d32f3242e08a982a2
j4sond3ak1n <jason.deakin.dev@gmail.com>
2017-12-08 22:35:52 +0000

    more content corrections

--- src/pages/mindfulness/mindfulness.ts
+++ src/pages/mindfulness/mindfulness.ts
@@ -13,8 +13,8 @@ export class MindfulnessPage {
   breatheAlert(event) {
       let alert = this.alertCtrl.create({
           title: 'Breathe Deeply',
-          subTitle: 'Meditation revolves arounds breathing. It\'s a great way to relax and centre yourself\. To begin practising, inhale for 3 seconds and exhale for 3 seconds. As you practice more, increase the amount of seconds\. ',
-          buttons: ['OK']
+          subTitle: 'Meditation revolves around breathing. It\'s a great way to relax and centre yourself\. To begin practising, inhale for 3 seconds and exhale for 3 seconds. As you practice more, increase the amount of seconds\. ',
+          buttons: ['x']
       })
       alert.present();
   }
@@ -23,7 +23,7 @@ export class MindfulnessPage {
       let alert = this.alertCtrl.create({
           title: 'Eat Well and Exercise',
           subTitle: 'Mindful eating involves paying full attention to the experience of eating and drinking, both inside and outside the body. Pay attention to the colors, smells, textures and flavours. Pay attention to the experience of the body. Avoid sitting for extended periods and move regularly during the day.  ',
-          buttons: ['OK']
+          buttons: ['x']
       })
       alert.present();
   }
@@ -32,7 +32,7 @@ export class MindfulnessPage {
       let alert = this.alertCtrl.create({
           title: 'Notice and Listen',
           subTitle: 'Be present in the moment. Allow your mind to let go and simply notice what\'s around you. Listen to what others are saying, attempt to avoid judgements and focus on the content of what they\'re saying ',
-          buttons: ['OK']
+          buttons: ['x']
       })
       alert.present();
   }
@@ -41,7 +41,7 @@ export class MindfulnessPage {
       let alert = this.alertCtrl.create({
           title: 'Practice Gratitude',
           subTitle: 'Being grateful is important habit to form. It reminds us to enjoy what we have, rather than desiring what we don\'t. Write down 3 things you\'re grateful for when you wake up and be specific.',
-          buttons: ['OK']
+          buttons: ['x']
       })
       alert.present();
   }
