629779abd4490f3ac4795a8076aff294fffaed11
j4sond3ak1n <jason.deakin.dev@gmail.com>
2017-12-09 20:57:48 +0000

    alert styling

--- src/pages/mentalhealthfeelslike/mentalhealthfeelslike.ts
+++ src/pages/mentalhealthfeelslike/mentalhealthfeelslike.ts
@@ -12,29 +12,20 @@ export class MentalhealthfeelslikePage {
   }
 
 }
-//
+
 // Memes = [
-//
-// {img src = "assets/img/NxLX5vUQacXXqSWykM9A_3.jpg"},
-// {img src = "assets/img/2QyaDTPRPukRP4SCi7em_5.jpg"},
-// {img src = "assets/img/KuqBwGN1TPSr6TYN4g4z_2.jpg"},
-// {img src = "assets/img/DUWeB8dgTSOqwd8M51jX_7.jpeg"},
-// {img src = "assets/img/X9SMH5tnSKyg9TbwQtQQ_1.jpg"},
-// {img src = "assets/img/BFlDFbRBe0B2pgOgZYfQ_6.jpg"},
-//   img
-//           src = "assets/img/mah2hAB8SGiMwow1zOSF_9.jpg"
-//             < img
-//           src = "assets/img/T8SicR6URjaLAr3gVk1g_10.jpg"
-//             < img
-//           src = "assets/img/PENiy5RQQYqrjUVecZcD_11.jpg"
-//             < img
-//           src = "assets/img/IKFo7sgSVG5jxrAI1das_14.jpeg"
-//             < img
-//           src = "assets/img/b5pxpdZfSqqqfHrf8S8t_12.jpg"
-//             < img
-//           src = "assets/img/DhIGBJ8R0mgH93QlOldg_15.jpeg"
-//             < img
-//           src = "assets/img/kWyGS92WQumQvj8uqZsm_13.jpg"
-//             < img
-//           src = "assets/img/sIJchlTTF2nlvceE45JS_16.jpg"
-//
+//   {img src = "assets/img/NxLX5vUQacXXqSWykM9A_3.jpg"},
+//   {img src = "assets/img/2QyaDTPRPukRP4SCi7em_5.jpg"},
+//   {img src = "assets/img/KuqBwGN1TPSr6TYN4g4z_2.jpg"},
+//   {img src = "assets/img/DUWeB8dgTSOqwd8M51jX_7.jpeg"},
+//   {img src = "assets/img/X9SMH5tnSKyg9TbwQtQQ_1.jpg"},
+//   {img src = "assets/img/BFlDFbRBe0B2pgOgZYfQ_6.jpg"},
+//   {img src = "assets/img/mah2hAB8SGiMwow1zOSF_9.jpg"}
+//   {img src = "assets/img/T8SicR6URjaLAr3gVk1g_10.jpg"}
+//   {img src = "assets/img/PENiy5RQQYqrjUVecZcD_11.jpg"}
+//   {img src = "assets/img/IKFo7sgSVG5jxrAI1das_14.jpeg"}
+//   {img src = "assets/img/b5pxpdZfSqqqfHrf8S8t_12.jpg"}
+//   {img src = "assets/img/DhIGBJ8R0mgH93QlOldg_15.jpeg"}
+//   {img src = "assets/img/kWyGS92WQumQvj8uqZsm_13.jpg"}
+//   {img src = "assets/img/sIJchlTTF2nlvceE45JS_16.jpg"}
+// ]
