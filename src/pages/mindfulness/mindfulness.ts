6f2a3ae6771b5bad8b5210b078c137151f0fa128
j4sond3ak1n <jason.deakin.dev@gmail.com>
2017-12-09 00:52:04 +0000

    more styling and ammendments

--- src/pages/mindfulness/mindfulness.ts
+++ src/pages/mindfulness/mindfulness.ts
@@ -12,7 +12,7 @@ export class MindfulnessPage {
 
   breatheAlert(event) {
       let alert = this.alertCtrl.create({
-          title: 'Breathe Deeply',
+          title: 'Breathe deeply',
           subTitle: 'Meditation revolves around breathing. It\'s a great way to relax and centre yourself\. To begin practising, inhale for 3 seconds and exhale for 3 seconds. As you practice more, increase the amount of seconds\. ',
           buttons: ['x']
       })
@@ -21,8 +21,8 @@ export class MindfulnessPage {
 
   exerciseAlert(event) {
       let alert = this.alertCtrl.create({
-          title: 'Eat Well and Exercise',
-          subTitle: 'Mindful eating involves paying full attention to the experience of eating and drinking, both inside and outside the body. Pay attention to the colors, smells, textures and flavours. Pay attention to the experience of the body. Avoid sitting for extended periods and move regularly during the day.  ',
+          title: 'Eat well and exercise',
+          subTitle: 'Mindful eating involves paying full attention to the experience of eating and drinking, both inside and outside the body. Pay attention to the colors, smells, textures and flavours. Pay attention to the experience of the body.  ',
           buttons: ['x']
       })
       alert.present();
@@ -30,7 +30,7 @@ export class MindfulnessPage {
 
   noticeAlert(event) {
       let alert = this.alertCtrl.create({
-          title: 'Notice and Listen',
+          title: 'Notice and listen',
           subTitle: 'Be present in the moment. Allow your mind to let go and simply notice what\'s around you. Listen to what others are saying, attempt to avoid judgements and focus on the content of what they\'re saying ',
           buttons: ['x']
       })
@@ -39,7 +39,7 @@ export class MindfulnessPage {
 
   gratitudeAlert(event) {
       let alert = this.alertCtrl.create({
-          title: 'Practice Gratitude',
+          title: 'Practice gratitude',
           subTitle: 'Being grateful is important habit to form. It reminds us to enjoy what we have, rather than desiring what we don\'t. Write down 3 things you\'re grateful for when you wake up and be specific.',
           buttons: ['x']
       })
