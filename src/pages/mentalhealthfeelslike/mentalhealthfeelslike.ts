871a6a68701bf05789fbefc0e7a593a11b20c703
j4sond3ak1n <jason.deakin.dev@gmail.com>
2017-12-10 10:14:01 +0000

    mental health feels like random meme button

--- src/pages/mentalhealthfeelslike/mentalhealthfeelslike.ts
+++ src/pages/mentalhealthfeelslike/mentalhealthfeelslike.ts
@@ -8,28 +8,32 @@ import {viewDebugError} from "@angular/core/src/view/errors";
 })
 export class MentalhealthfeelslikePage {
 
+  Memes = [
+    {img: "assets/img/NxLX5vUQacXXqSWykM9A_3.jpg"},
+    {img: "assets/img/2QyaDTPRPukRP4SCi7em_5.jpg"},
+    {img: "assets/img/KuqBwGN1TPSr6TYN4g4z_2.jpg"},
+    {img: "assets/img/DUWeB8dgTSOqwd8M51jX_7.jpeg"},
+    {img: "assets/img/X9SMH5tnSKyg9TbwQtQQ_1.jpg"},
+    {img: "assets/img/BFlDFbRBe0B2pgOgZYfQ_6.jpg"},
+    {img: "assets/img/mah2hAB8SGiMwow1zOSF_9.jpg"},
+    {img: "assets/img/T8SicR6URjaLAr3gVk1g_10.jpg"},
+    {img: "assets/img/PENiy5RQQYqrjUVecZcD_11.jpg"},
+    {img: "assets/img/IKFo7sgSVG5jxrAI1das_14.jpeg"},
+    {img: "assets/img/b5pxpdZfSqqqfHrf8S8t_12.jpg"},
+    {img: "assets/img/DhIGBJ8R0mgH93QlOldg_15.jpeg"},
+    {img: "assets/img/kWyGS92WQumQvj8uqZsm_13.jpg"},
+    {img: "assets/img/sIJchlTTF2nlvceE45JS_16.jpg"}
+  ]
+
+  imageSource: String;
+
   constructor(public navCtrl: NavController) {
+    this.randomMeme();
   }
 
-}
+  randomMeme() {
+    let meme = this.Memes[Math.floor(Math.random() * this.Memes.length)];
+    this.imageSource = meme.img;
 
-// Memes = [
-//   {img src = "assets/img/NxLX5vUQacXXqSWykM9A_3.jpg"},
-//   {img src = "assets/img/2QyaDTPRPukRP4SCi7em_5.jpg"},
-//   {img src = "assets/img/KuqBwGN1TPSr6TYN4g4z_2.jpg"},
-//   {img src = "assets/img/DUWeB8dgTSOqwd8M51jX_7.jpeg"},
-//   {img src = "assets/img/X9SMH5tnSKyg9TbwQtQQ_1.jpg"},
-//   {img src = "assets/img/BFlDFbRBe0B2pgOgZYfQ_6.jpg"},
-//   {img src = "assets/img/mah2hAB8SGiMwow1zOSF_9.jpg"}
-//   {img src = "assets/img/T8SicR6URjaLAr3gVk1g_10.jpg"}
-//   {img src = "assets/img/PENiy5RQQYqrjUVecZcD_11.jpg"}
-//   {img src = "assets/img/IKFo7sgSVG5jxrAI1das_14.jpeg"}
-//   {img src = "assets/img/b5pxpdZfSqqqfHrf8S8t_12.jpg"}
-//   {img src = "assets/img/DhIGBJ8R0mgH93QlOldg_15.jpeg"}
-//   {img src = "assets/img/kWyGS92WQumQvj8uqZsm_13.jpg"}
-//   {img src = "assets/img/sIJchlTTF2nlvceE45JS_16.jpg"}
-// // ]
-//
-// randomMeme(){
-//   let meme = this.Memes[Math.floor(Math.random()*this.Quotes.length)];
-//   this.quoteText = quote.quote;
+  }
+}
